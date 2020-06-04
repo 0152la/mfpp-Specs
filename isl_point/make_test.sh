404a17384aed9ba8717b1b3b192f9fbabd69e398
Andrei Lascu <al2510@imperial.ac.uk>
2020-06-04 13:41:46 +0100

    Remove redundant files

--- isl_point/make_test.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p output
-./build/mtFuzzer `pwd`/isl_point/isl_point.cpp -o `pwd`/output/test.cpp --lib-list=`pwd`/isl_point/isl-noexceptions-point.h --set-meta-path=`pwd`/isl_point/set_meta_tests_isl.yaml
