4d60959877116ed61f22665b73757806adf2897e
Andrei Lascu <al2510@imperial.ac.uk>
2020-09-14 13:23:32 +0100

    Update `spec_fuzz_point.hpp`

--- isl_point/spec_fuzz_point.hpp
+++ isl_point/spec_fuzz_point.hpp
@@ -1,9 +1,8 @@
 #ifndef _SPEC_FUZZ_HPP
 #define _SPEC_FUZZ_HPP
 
-#ifndef TEST
-#include "isl-noexceptions-point.h"
-#include "set_meta_tests_isl.hpp"
+#ifdef TEST
+#include "isl_spec_defs.h"
 #else
 #include "isl-noexceptions.h"
 #endif
@@ -13,20 +12,13 @@
 
 namespace fuzz
 {
-    namespace input
-    {
-        static int dims = 5;
-        static int cons = 20;
-    }
-
-    static isl::set output_var;
+    static OUT_VAR_TYPE output_var;
 
     void start() {}
     void end()   {}
 
     template <class T> T fuzz_new();
-    //isl::set fuzz_new(std::string type_name);
-    template<typename T> T fuzz_rand(T min, T max);
+    template<typename T, typename U> T fuzz_rand(U min, U max);
 
     void meta_test() {}
 }
