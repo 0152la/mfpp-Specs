fb5f977da5e64174dfe3ea8398ed02d826f3153f
Andrei Lascu <al2510@imperial.ac.uk>
2020-11-26 14:11:16 +0000

    Replace isl_point runtime with cmake

--- isl_point/runtime/compile_isl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -eq 1 ]
-then
-    test_name=$1
-else
-    test_name="test.cpp"
-fi
-echo "Compiling $test_name"
-g++ -std=c++17 -DTEST -g -o ${test_name%.*} $test_name $(pkg-config --env-only --cflags --libs isl)
