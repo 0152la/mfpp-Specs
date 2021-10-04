ad990a28562fd24b9f316a86a3e6939b6c20949b
Andrei Lascu <al2510@imperial.ac.uk>
2021-10-04 18:44:40 +0100

    Some temporary spec re-adding
    * re-added a beta version of update Yices spec
    * re-added old, but working Boolector spec

--- SMT_QF_BV/boolector/boolector_fuzz_type_defs.hpp
+++ SMT_QF_BV/boolector/boolector_fuzz_type_defs.hpp
@@ -1,8 +1,5 @@
 namespace fuzz
 {
-    typedef term_t bool_term;
-    typedef term_t bv_term;
-
     class FreeVars {
         public:
             fuzz::bv_term vars[FV_COUNT];
@@ -14,10 +11,5 @@ namespace fuzz
             fuzz::FreeVars& fvs;
 
             fuzz_context(fuzz::FreeVars& _fvs) : fvs(_fvs) {} ;
-
-            term_t simplify(term_t t)
-            {
-                return t;
-            }
     };
 } // namespace fuzz
---
+++ SMT_QF_BV/boolector/boolector_spec_defs.hpp
@@ -0,0 +1,23 @@
+#ifndef _BOOLECTOR_SPEC_DEFS_HPP
+#define _BOOLECTOR_SPEC_DEFS_HPP
+
+#include "boolector_mfpp.h"
+
+typedef BoolectorNode* OUT_VAR_TYPE;
+
+//#include "meta_spec_boolector.hpp"
+#include "set_meta_tests_boolector.hpp"
+
+namespace fuzz
+{
+    static BoolectorNode* output_var;
+    BoolectorNode* output_var_get(size_t);
+} // namespace fuzz
+
+namespace lib_helper_funcs {
+
+} // namespace lib_helper_funcs
+
+#endif // _BOOLECTOR_SPEC_DEFS_HPP
+
+
